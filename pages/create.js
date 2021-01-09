48b099e2421f46c55063930782df7ae571910cfd
minna.sho <minaashokouhi@gmail.com>
2021-01-09 21:15:31 +0330

    Implement the create post functionality

--- pages/create.js
+++ pages/create.js
@@ -1,3 +1,96 @@
-export default function FirstPost() {
-    return <h1>First Post</h1>
+import React, {useState} from "react";
+import {Formik} from "formik";
+import * as Yup from "yup";
+import handleFormikBlur from "../lib/handleBlur";
+import {Button, Container, Form, Spinner} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function doServerSideStuff(values, resetForm, setSubmitting) {
+    const fs = require('browserify-fs');
+    console.log('called');
+    // write to a new file named [title].json
+    const fileContent = JSON.stringify(values, null, 2);
+    console.log(fileContent);
+    fs.writeFile(`/posts/${fileContent.title}.json`, fileContent);
+    setSubmitting(false);
+    resetForm({});
+
 }
+
+export default function Create() {
+
+    const [inputs, setInputs] = useState({
+        title: '',
+        content: '',
+        date: ''
+    });
+
+    const validationSchema = Yup.object({
+        title: Yup.string()
+            .required('Required field'),
+        content: Yup.string()
+            .required("Required field"),
+    });
+
+    const doSubmit = (values, {resetForm, setSubmitting}) => {
+        console.log(values);
+        let currentDate = JSON.stringify(new Date());
+        console.log(currentDate);
+        values.date = currentDate.substr(1, 10);
+        setSubmitting(true);
+        doServerSideStuff(values, resetForm, setSubmitting);
+    }
+
+    return (
+        <Formik enableReinitialize={true}
+                initialValues={inputs}
+                validationSchema={validationSchema}
+                onSubmit={doSubmit}>
+            {({handleChange, handleSubmit, isSubmitting, handleBlur, touched, values, errors}) => (
+                <Container><Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="title">
+                        <Form.Label>
+                            Title
+                        </Form.Label>
+                        <Form.Control type="text" name='title'
+                                      placeholder='Title'
+                                      onChange={handleChange}
+                                      onBlur={(event) => handleFormikBlur(event, handleBlur)}
+                                      value={values.title}/>
+                        {(touched.title && errors.title) ?
+                            <small>{errors.title}</small> : null}
+                    </Form.Group>
+                    <Form.Group controlId="date" className='d-none'>
+                        <Form.Label>
+                            Date
+                        </Form.Label>
+                        <Form.Control type="date-time" name='date'
+                                      placeholder='date'
+                                      onChange={handleChange}
+                                      onBlur={(event) => handleFormikBlur(event, handleBlur)}
+                                      value={values.date}/>
+                        {(touched.date && errors.date) ?
+                            <small>{errors.date}</small> : null}
+                    </Form.Group>
+                    <Form.Group controlId="content">
+                        <Form.Label>
+                            Content
+                        </Form.Label>
+                        <Form.Control type="text" as="textarea" rows={8}
+                                      name='content'
+                                      value={values.content}
+                                      onChange={handleChange}
+                                      onBlur={(event) => handleFormikBlur(event, handleBlur)}/>
+                        {(touched.content && errors.content) ?
+                            <small>{errors.content}</small> : null}
+                    </Form.Group>
+                    <Button variant="primary" type="submit"
+                            disabled={(isSubmitting || !!errors.content || !!errors.title)}>
+                        {isSubmitting ?
+                            <Spinner animation="border" variant="secondary" size='sm'/> : 'Submit'}
+                    </Button>
+                </Form></Container>
+            )}
+        </Formik>
+    );
+};
